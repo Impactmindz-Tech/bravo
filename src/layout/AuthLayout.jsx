90d7949d016581e36db34a30347008eee3561114
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-06-24 18:33:28 +0530

    some chnage

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import auth_img from '../assets/images/authImage.jpg'
 
 const AuthLayout = ({ children }) => {
     return (
         <div className="flex w-full sm:justify-center h-screen">
             <div className="w-[45%] bg-blue-900 flex items-center justify-center sm:hidden">
-                <img src="loginLeftSideImage.png" alt="login left side image" className="w-full h-full  object-contain" />
+                <img src={auth_img} alt="login left side image" className="max-w-[65%]  " />
             </div>
             <div className="flex-1 flex items-center justify-center">
                 {children}
