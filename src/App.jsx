460e072c0eb91e568ca431fbc9947f0cd19bb359
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-29 17:26:34 +0530

    solving deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Routing from "./route/Routing";
 import router from "./route/Routing";
 import { Toaster } from 'react-hot-toast';
-import "react-country-state-city/dist/react-country-state-city.css";
 
 
 function App() {
