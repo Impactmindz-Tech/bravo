b5c47f8dcf7a5be547fb6ba446ac1870284eecee
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-31 15:47:36 +0530

    view group modal size increased

--- src/components/Modal/ViewGroupModal.jsx
+++ src/components/Modal/ViewGroupModal.jsx
@@ -1,17 +1,15 @@
 import { IoMdClose } from "react-icons/io";
 import { Modal } from "@mui/material";
-import adminUserProfile from "../../assets/images/adminUserProfile.svg";
 // eslint-disable-next-line react/prop-types
 const ViewGroupModal = ({ viewModalState, setViewModalState, viewModalData }) => {
   return (
-    <Modal open={viewModalState} onClose={() => setViewModalState(false)} className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto backdrop-filter   bg-opacity-50">
-      <div className="h-[600px] overflow-y-auto sm:h-[50vh] mainFormSection md:h-[50vh] lg:h-[50vh] xl:h-[50vh]  2xl:h-[50vh] 3xl:h-[50vh] 4xl:h-[40vh]">
-        <div className="relative w-[100%] max-w-[55vw] sm:max-w-[100vw] md:max-w-[100vw] lg:max-w-[70vw] xl:max-w-[65vw] 2xl:max-w-[65vw] 3xl:max-w-[85vw] 4xl:max-w-[65vw] mx-auto rounded-lg overflow-hidden sm:w-[90vw] md:w-[90vw] lg:w-[96vw]">
-          <div className="relative bg-white  rounded-lg shadow-md pb-2 ">
-            {/* top model section */}
-            <div className="flex justify-between items-center  bg-blue-900 py-2">
+    <Modal open={viewModalState} onClose={() => setViewModalState(false)} className="fixed modalContainer inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
+      <div className="overflow-y-auto mainFormSection sm:w-[90vw] sm:h-[70vh] md:w-[90vw] md:h-[70vh] lg:w-[80vw] lg:h-[65vh] xl:w-[60vw] xl:h-[60vh] w-[55vw] h-[60vh]">
+        <div className="relative w-full mx-auto rounded-lg overflow-hidden">
+          <div className="relative bg-white rounded-lg shadow-md pb-4">
+            <div className="flex justify-between items-center mb-4 bg-blue-900 py-2">
               <h2 className="text-xl font-semibold text-gray-800 pl-4 text-white">{viewModalData?.name}</h2>
-              <button onClick={() => setViewModalState(false)} className="text-red text-white  hover:text-gray-900 hover:outline-none border-none outline-none bg-blue-900 text-lg">
+              <button onClick={() => setViewModalState(false)} className="bg-blue-900 hover:text-gray-900 hover:border-none hover:outline-none text-lg text-white border-none outline-none">
                 <IoMdClose />
               </button>
             </div>
@@ -24,8 +22,8 @@ const ViewGroupModal = ({ viewModalState, setViewModalState, viewModalData }) =>
                     {/* table heading */}
                     <thead>
                       <tr>
-                        <th className="px-4 py-4   bg-white top-0 border-gray-200 text-left rounded-tl-lg w-[180px]">First Name</th>
-                        <th className="text-left   bg-white border-gray-200  px-4">Last Name</th>
+                        <th className="px-4 py-4   bg-white top-0 border-gray-200 text-center rounded-tl-lg w-[180px]">First Name</th>
+                        <th className="text-center   bg-white border-gray-200  px-4">Last Name</th>
                         <th className=" text-center  bg-white border-gray-200 px-9">Username</th>
                         <th className=" text-center bg-white border-gray-200">Email</th>
                       </tr>
@@ -33,6 +31,42 @@ const ViewGroupModal = ({ viewModalState, setViewModalState, viewModalData }) =>
                     {/* table body */}
                     <tbody>
                       {/* data 1 */}
+                      {viewModalData?.data?.length > 0 && (
+                        <>
+                          {viewModalData.data.map((item) => (
+                            <tr className="text-center" key={item.user_id}>
+                              <td className="px-4 py-2 sm:max-w-[200px]">{item.first_name}</td>
+                              <td className="px-4 py-2">{item.last_name}</td>
+                              <td className="px-4 py-2">{item.username}</td>
+                              <td className="px-4 py-2 capitalize">{item.email}</td>
+                            </tr>
+                          ))}
+                        </>
+                      )}
+                      {viewModalData?.data?.length > 0 && (
+                        <>
+                          {viewModalData.data.map((item) => (
+                            <tr className="text-center" key={item.user_id}>
+                              <td className="px-4 py-2 sm:max-w-[200px]">{item.first_name}</td>
+                              <td className="px-4 py-2">{item.last_name}</td>
+                              <td className="px-4 py-2">{item.username}</td>
+                              <td className="px-4 py-2 capitalize">{item.email}</td>
+                            </tr>
+                          ))}
+                        </>
+                      )}
+                      {viewModalData?.data?.length > 0 && (
+                        <>
+                          {viewModalData.data.map((item) => (
+                            <tr className="text-center" key={item.user_id}>
+                              <td className="px-4 py-2 sm:max-w-[200px]">{item.first_name}</td>
+                              <td className="px-4 py-2">{item.last_name}</td>
+                              <td className="px-4 py-2">{item.username}</td>
+                              <td className="px-4 py-2 capitalize">{item.email}</td>
+                            </tr>
+                          ))}
+                        </>
+                      )}
                       {viewModalData?.data?.length > 0 && (
                         <>
                           {viewModalData.data.map((item) => (
